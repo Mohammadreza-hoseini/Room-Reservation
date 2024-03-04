8fdb7b729479809eaf10dee2da85a90a84f0127d
mohammad-hoseini <mohammadrezajalahoseini@yahoo.com>
2024-03-04 19:21:22 +0330

    add admin panel and change relation on reservation table for user field

--- reservation/admin.py
+++ reservation/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Calendar, Room, Reservation
 
 # Register your models here.
+
+admin.site.register(Calendar)
+admin.site.register(Room)
+admin.site.register(Reservation)
