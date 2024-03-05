4daedc3c80f70b0e4acb81be7b05f6067e01546e
BehnamAslani <BehnamAslani@proton.me>
2024-03-05 21:20:47 +0330

    test

--- reservation/views.py
+++ reservation/views.py
@@ -1,2 +1,10 @@
 from django.shortcuts import render
+from .models import * 
+from django.views.generic import ListView
 
+class EmptyRoomListView(ListView):
+
+    model = Room
+    context_object_name = 'empty_room_list'
+    queryset = Room.objects.filter(status=False)
+    
