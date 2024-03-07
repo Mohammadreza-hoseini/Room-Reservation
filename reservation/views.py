45ae3bcb0216e0b585c421ff6ac4036b7d8c705f
BehnamAslani <BehnamAslani@proton.me>
2024-03-07 18:29:23 +0330

    make RoomStatusView for status of rooms

--- reservation/views.py
+++ reservation/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from .models import * 
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
 class EmptyRoomListView(ListView):
 
@@ -10,3 +10,17 @@ class EmptyRoomListView(ListView):
     
     def get_queryset(self):
         return Room.objects.filter(status=False)
+    
+
+class RoomStatusView(DetailView):
+    model = Room
+    template_name = 'room_status.html'
+    context_object_name = 'room'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        room = self.get_object()
+        reservation_status = "Reserved" if room.status else "Empty"
+        context['reservation_status'] = reservation_status
+        return context
+    
