00cbdaf7ad17d8ca2ee4ddf9c03db7853d029656
arman <shakerian.arman@yahoo.com>
2024-03-15 10:54:48 +0330

    debug: reservationModelTest label methods

--- reservation/tests/test_models.py
+++ reservation/tests/test_models.py
@@ -11,7 +11,7 @@ class ReservationModelTest(TestCase):
         # all_rooms = Room.objects.all().count()
         # print("Debug:")
         # print(all_rooms)
-        
+
         room_obj = Room.objects.get(title="first room")
         teamLeader_obj = TeamLeader.objects.get(leader__username="mohammad")
         Reservation.objects.create(room_id=room_obj, leader=teamLeader_obj)
@@ -19,9 +19,9 @@ class ReservationModelTest(TestCase):
     def test_room_id_label(self):
         reservation_obj = Reservation.objects.get(id=1)
         field_label = reservation_obj._meta.get_field("room_id").verbose_name
-        self.assertEqual(field_label, "room_id")
+        self.assertEqual(field_label, "choose room")
 
     def test_leader_label(self):
         reservation_obj = Reservation.objects.get(id=1)
         field_label = reservation_obj._meta.get_field("leader").verbose_name
-        self.assertEqual(field_label, "leader")
+        self.assertEqual(field_label, "choose leader")
