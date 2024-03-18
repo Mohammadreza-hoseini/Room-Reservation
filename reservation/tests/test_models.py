b2dba58df6db197bb5aa2b09c2cc2b2b3f2f06c3
arman <shakerian.arman@yahoo.com>
2024-03-18 13:30:31 +0330

    debug: change res_id + no set_up

--- reservation/tests/test_models.py
+++ reservation/tests/test_models.py
@@ -6,17 +6,13 @@ from accounts.models import TeamLeader
 
 class ReservationModelTest(TestCase):
 
-    fixtures = ["newUser_data.json", "teamLeader_data.json", "calendar_data.json", "rooms_data.json", "reservation_data.json"]
-
-    # @classmethod
-    # def setUpTestData(cls):
-    #     # all_rooms = Room.objects.all().count()
-    #     # print("Debug:")
-    #     # print(all_rooms)
-
-    #     room_obj = Room.objects.get(title="test_room_1")
-    #     teamLeader_obj = TeamLeader.objects.get(leader__username="mohammad")
-    #     Reservation.objects.create(room_id=room_obj, leader=teamLeader_obj)
+    fixtures = [
+        "newUser_data.json",
+        "teamLeader_data.json",
+        "calendar_data.json",
+        "rooms_data.json",
+        "reservation_data.json",
+    ]
 
     def test_room_id_label(self):
         reservation_obj = Reservation.objects.get(id=2001)
@@ -24,6 +20,6 @@ class ReservationModelTest(TestCase):
         self.assertEqual(field_label, "choose room")
 
     def test_leader_label(self):
-        reservation_obj = Reservation.objects.get(id=1)
+        reservation_obj = Reservation.objects.get(id=2001)
         field_label = reservation_obj._meta.get_field("leader").verbose_name
         self.assertEqual(field_label, "choose leader")
