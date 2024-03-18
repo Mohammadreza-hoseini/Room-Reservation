c980f62bc21e5764dd431968e5cfea21efe3d4b9
arman <shakerian.arman@yahoo.com>
2024-03-18 20:30:47 +0330

    import Room model

--- reservation/tests/test_views.py
+++ reservation/tests/test_views.py
@@ -1,8 +1,20 @@
 from django.test import TestCase
 
 
+from reservation.models import Room
+
+
 class testRoomDetailView(TestCase):
-    fixtures = ["rooms_data.json"]
+    fixtures = [
+        "newUser_data.json",
+        "calendar_data.json",
+        "rooms_data.json",
+    ]
 
     # TODO
     # use client obj in TestCase
+    def test_post_method(self):
+        room_id = 1001
+        url = f"room_detail/{room_id}/"
+        # TODO
+        # response = self.client.get(url, data=)
